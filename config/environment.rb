0b3860dadd887c51575a14f32366b4eb697e241e
Faith Gicheha <fwgicheh@gmail.com>
2020-07-07 23:43:38 -0700

    moved environment file

---
+++ config/environment.rb
@@ -0,0 +1,18 @@
+#required gems 
+require "httparty"
+require "pry"
+require "travel_info/version"
+
+# module TravelInfo
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+# require "lingo/version"
+# require 'bundle/setup'#make sure all gems in the Gemfile are present on this machine 
+# Bundle.require
+
+
+
+require_relative "../lib/api.rb"
+require_relative "../lib/cli.rb"
+# require_relative "../lib/country_info.rb"
