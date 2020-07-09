a5623723515ac26176daa0b5e66e4cc415189fdf
Faith Gicheha <fwgicheh@gmail.com>
2020-07-08 20:53:32 -0700

    add lib folder classes and files

--- lib/api.rb
+++ lib/api.rb
@@ -1,1 +1,8 @@
 # get data from the internet using scraping or hitting an api
+
+class API 
+def get_info
+  puts "it's working from API#get_info"
+end 
+
+end 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,1 +1,17 @@
-# primary job of the CLI class is to providde the user infterface - ‘gets’ and ‘puts’
+# primary job of the CLI class is to provide the user infterface - ‘gets’ and ‘puts’
+
+class CLI 
+
+  attr_reader :api
+
+  def initialize
+    @api = API.new
+  end 
+
+  def start 
+    self #what is self? in an instance method, it's an instance- the instance used to call the method
+    
+    api.get_info
+  end 
+  
+end 
