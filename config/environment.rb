b98f2833aeae76d213e0b1b568509c7301d989dd
Faith Gicheha <fwgicheh@gmail.com>
2020-07-08 20:50:47 -0700

    add bundler/setup command

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,17 @@
 #required gems 
 require "httparty"
 require "pry"
-require "travel_info/version"
+# require "travel_info/version"
 
-# module TravelInfo
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
+
 # require "lingo/version"
-# require 'bundle/setup'#make sure all gems in the Gemfile are present on this machine 
-# Bundle.require
+require 'bundler/setup' #make sure all gems in the Gemfile are present on this machine 
+Bundler.require #require at runtime
 
-
+#require_relative your code
 
 require_relative "../lib/api.rb"
 require_relative "../lib/cli.rb"
-# require_relative "../lib/country_info.rb"
+# require_relative "../lib/travel.rb"
+
+puts "Hello"
