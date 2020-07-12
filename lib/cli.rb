055d593a6207440c82494df2a71a921ba824b723
Faith Gicheha <fwgicheh@gmail.com>
2020-07-12 15:27:34 -0700

    modify my def countries_list function and added color to my ui function.

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,34 +10,38 @@ class CLI
     
   def ui
     space 
-    print "Welcome to your travel guide."
+    print "Welcome to your travel guide.".yellow 
     space
-    print "When you select a language from the menu below, the program will display a list of countries that speak that langauge."
+    print "When you select a language from the menu below, the program will display a list of countries that speak that langauge.".yellow
     space
 
     puts "\nWhat language do you want to learn about? Please select a number from the menu and press enter"
-      print " \n 1.Arabic \n 2.English\n 3.French \n 4.Spanish\n\n"
+      print " \n 1.Arabic \n 2.English\n 3.French \n 4.Spanish\n\n".cyan
     user_input = gets.strip
-    # validate_in
+ 
     if user_input == "1"
       @api.get_info("ar")
       puts countries_list
+
     
     puts "Wrong input, please try again!"
-     next #equivalent of 'continue' in java? 
+     #equivalent of 'continue' in java? 
     end
     puts "\nWould you like to learn about a different language? press 'y' to continue press any key to exit"
     user_input = gets.strip.to_s.downcase
     if user_input == "y"
-     next 
+     
     else 
       puts "Thank you, See you later!" 
-      break 
+   
     end  
     end 
-  end 
+
     def countries_list  
-      puts @travel.all
+       response = @api.response
+       @travel.get_data(response)
+       puts @travel.all
+      @api.response.clear
     end
     
   def space 
