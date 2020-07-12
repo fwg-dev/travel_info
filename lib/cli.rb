95303096f5c78d228f845e7d92eb7b065c2f68a5
Faith Gicheha <fwgicheh@gmail.com>
2020-07-12 15:36:13 -0700

    pull from api.rb and travel.rb file. final code works

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,7 +14,7 @@ class CLI
     space
     print "When you select a language from the menu below, the program will display a list of countries that speak that langauge.".yellow
     space
-
+  while(true)
     puts "\nWhat language do you want to learn about? Please select a number from the menu and press enter"
       print " \n 1.Arabic \n 2.English\n 3.French \n 4.Spanish\n\n".cyan
     user_input = gets.strip
@@ -22,21 +22,31 @@ class CLI
     if user_input == "1"
       @api.get_info("ar")
       puts countries_list
-
-    
-    puts "Wrong input, please try again!"
-     #equivalent of 'continue' in java? 
+    elsif user_input == "2"
+      @api.get_info("en")
+      puts countries_list 
+    elsif user_input == "3"
+      @api.get_info("fr")
+      puts countries_list 
+    elsif user_input == "4"
+      @api.get_info("es")
+      puts countries_list 
+    else 
+  
+    puts "Wrong input, please try again!".red
+    next #equivalent of 'continue' in java? 
     end
+   
     puts "\nWould you like to learn about a different language? press 'y' to continue press any key to exit"
     user_input = gets.strip.to_s.downcase
     if user_input == "y"
-     
+     next
     else 
-      puts "Thank you, See you later!" 
-   
+      puts "Thank you, See you later!". yellow 
+      break
     end  
     end 
-
+  end 
     def countries_list  
        response = @api.response
        @travel.get_data(response)
