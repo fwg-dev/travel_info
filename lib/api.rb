9f851df4ec8c93f06fff120d1388143c32220087
Faith Gicheha <fwgicheh@gmail.com>
2020-07-12 15:15:36 -0700

    modified and reconstructed HTTParty.get response

--- lib/api.rb
+++ lib/api.rb
@@ -1,26 +1,23 @@
 # get data from the internet using scraping or hitting an api
 
 class API 
-
+  attr_accessor :response
   BASE_URL = "https://restcountries.eu/rest/v2/lang/"
 
+  def initialize
+    @response = []
+  end
 
-def get_info(selected_language_code)
+  def get_info(selected_language_code)
   #this argument is needed #get requests the server to return either a collection or a single item of collection 
-
- response = HTTParty.get(BASE_URL + selected_language_code)
- countries = []
- response.select {|i| countries.push(i["name"])}
-
-return countries #this prints out the list of countries when called upon 
-
-#  binding.pry
+ @response = HTTParty.get(BASE_URL + selected_language_code)  
  
-end
-
+  end
 end 
 
+# puts countries #this prints out the list of countries when called upon 
 
+#  binding.pr
 # print_countries =  response.select {|i| countries.push(i["name"])}
 
 # get_info(selected_language_code)
