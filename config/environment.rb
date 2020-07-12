9848063609efacbe52a6e4ae00b55afc06d10479
Faith Gicheha <fwgicheh@gmail.com>
2020-07-12 15:21:46 -0700

    modified the environement.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -2,16 +2,12 @@
 require "httparty"
 require "pry"
 require "colorize"
-# require "travel_info/version"
 
 
-# require "lingo/version"
 require 'bundler/setup' #make sure all gems in the Gemfile are present on this machine 
 Bundler.require #require at runtime
 
 #require_relative your code
-
 require_relative "../lib/api.rb"
 require_relative "../lib/cli.rb"
 require_relative "../lib/travel.rb"
-
